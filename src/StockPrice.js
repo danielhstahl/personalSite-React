3e674f4a385dc3371983985c701f75dc0cc99f0e
Daniel Stahl <danstahl1138@gmail.com>
2017-01-02 16:02:43 -0600

    edited stockprice

--- src/StockPrice.js
+++ src/StockPrice.js
@@ -1,22 +1,45 @@
 
 import React, { Component } from 'react';
 
-const Ajax = require('simple-ajax');
-    
+//const Ajax = require('simple-ajax');
+
+const ajax=(url)=>{
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'url');
+    xhr.setRequestHeader('Content-Type', 'text/plain');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            alert('User\'s name is ' + xhr.responseText);
+        }
+        else {
+            alert('Request failed.  Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send();
+}
+
+
+
 export default class StockPrice extends Component{
     constructor(props){
         super(props);
         this.state={
             stockvalue:""
         };
+        //console.log(location.hostname);
     }
     componentWillMount(){
+        //console.log();
+        const url="http://finance.yahoo.com/webservice/v1/symbols/"+this.props.stock+"/quote?format=json";
         var ajax = new Ajax({
-            url:'http://finance.google.com/finance/info?client=ig&q='+this.props.stock+'&callback=?',
+            url:url,//'http://finance.google.com/finance/info?client=ig&q='+this.props.stock+'&callback=?',
             dataType:'jsonp',
             method:'get',
+            cors:true,
+            //crossDomain: true,
             contentType:false,
-            requestedWith:false
+            requestedWith:false,
+            //headers:{/*"Access-Control-Allow-Credentials":true, "Access-Control-Allow-Origin":"*", "Access-Control-Allow-Headers":"*", */"Content-Type":"application/x-www-form-urlencoded"}
         });
         ajax.on('success', (event)=>{
             var data=event.target.response;
